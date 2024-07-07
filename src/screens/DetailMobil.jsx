ec71208d6fb46e4e513e899e02e629af3fb3ad69
Yayukk <yayukoktafiana27@gmail.com>
2024-07-07 15:22:45 +0700

    mendownload react native navigation dan menambahkan halaman file

---
+++ src/screens/DetailMobil.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const DetailMobil = () => {
+  return (
+    <View>
+      <Text>DetailMobil</Text>
+    </View>
+  );
+};
+
+export default DetailMobil;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/Pemesanan.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Pemesanan = () => {
+  return (
+    <View>
+      <Text>Pemesanan</Text>
+    </View>
+  );
+};
+
+export default Pemesanan;
+
+const styles = StyleSheet.create({});
